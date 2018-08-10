14aa1715e68ed5bc31cbfca6eb09ce5bf4640c10
Alexander <epple.alexander@web.de>
2018-08-10 23:56:45 +0200

    Additional inital stuff

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
