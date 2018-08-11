9e1af2ecd25f1e287b651b21a6d69aea64d7b25b
Alexander <epple.alexander@web.de>
2018-08-11 15:36:57 +0200

    Added camera movement rules

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,12 +2,21 @@
 
 public class Camera : MonoBehaviour {
 
+    [Header("Settings")]
     [SerializeField]
     private Transform target;
     [SerializeField]
-    private float lerpSpeed;
+    private float lerpSpeedX;
+    [SerializeField]
+    private float lerpSpeedY;
 
-	void Update () {
-        transform.SetPositionAndRotation(Vector3.Lerp(transform.position, target.position, lerpSpeed), Quaternion.identity);
+    void Update () {
+        // Lerp to position
+        Vector3 newPos =    new Vector3(Vector3.Lerp(transform.position, target.position, lerpSpeedX * Time.deltaTime).x,
+                                        Vector3.Lerp(transform.position, target.position, lerpSpeedY * Time.deltaTime).y);
+        // Only positive movement allowed
+        newPos.x = newPos.x > transform.position.x ? newPos.x : transform.position.x;
+        // Set position
+        transform.SetPositionAndRotation(newPos, Quaternion.identity);        
 	}
 }
