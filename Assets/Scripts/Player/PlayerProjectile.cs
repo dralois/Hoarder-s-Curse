0c299ef7faaf6fafe68cdfdfa3e4ad0d62921a4d
Alexander <epple.alexander@web.de>
2018-08-13 22:35:40 +0200

    Ranged weapon

---
+++ Assets/Scripts/Player/PlayerProjectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+
+    [Header("Movement")]
+    [SerializeField]
+    public float moveSpeed;
+
+    public int damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            EnemyFlying fly = collision.GetComponent<EnemyFlying>();
+            EnemyGround ground = collision.GetComponent<EnemyGround>();
+            if (fly == null)
+                ground.ApplyDamage(damage);
+            else
+                fly.ApplyDamage(damage);
+        }
+
+        if (collision.tag == "Enemy" || collision.tag == "Ground" || collision.tag == "Wall")
+            Destroy(gameObject);
+    }
+}
